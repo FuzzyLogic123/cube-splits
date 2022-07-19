77badff96d042b803ca623dea942667aa2670e97
carefulcactus <stephencurryallstar@gmail.com>
2022-07-19 22:20:00 +1000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import background from './hughs-amazing-background.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-[#001220] h-[2300px] overflow-hidden'>
+      {/* <img src={background} className="fixed z-[-1] h-screen object-cover w-screen"/> */}
+      <div className=""></div>
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <>
+            <div className="bg-no-repeat bg-center bg-cover bg-[url('/public/layered-waves-haikei.svg')] w-screen h-[50vh] flex items-center justify-center">
+                <h1 className="text-[#FFB1B1] xl:text-9xl text-5xl font-quantico text-center">
+                    Cube Splits
+                </h1>
+            </div>
+
+        </>
+    );
+}
+
+export default Header;
