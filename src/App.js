cddfd94afcf3b50b715689f774e37ef6cfb1e092
FuzzyLogic123 <pat.e1@icloud.com>
2022-07-20 19:41:08 +1000

    added arrow svg

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
 import background from './hughs-amazing-background.png';
+import DownScrollButton from './Components/DownScrollButton';
 
 function App() {
   return (
     <div className='bg-[#001220] h-[2300px] overflow-hidden'>
       {/* <img src={background} className="fixed z-[-1] h-screen object-cover w-screen"/> */}
-      <div className=""></div>
+      <div className="" />
       <Header />
+      <DownScrollButton />
     </div>
   );
 }
---
+++ src/Components/DownScrollButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ReactComponent as DownArrow } from '../Icons/down-arrow.svg';
+
+
+function DownScrollButton() {
+    return ( 
+            <DownArrow className='h-20 m-auto'/>
+     );
+}
+
+export default DownScrollButton;
