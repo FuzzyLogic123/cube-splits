123f129c65092d0cf306a51dd388d000cec9257d
FuzzyLogic123 <pat.e1@icloud.com>
2022-07-20 22:06:36 +1000

    more commits

--- src/Components/InputField.js
+++ src/Components/InputField.js
@@ -2,7 +2,7 @@ import React from 'react';
 function InputField() {
     return ( 
         <div className='w-screen text-center my-24'>
-            <input type="text" className='h-16 w-3/4 xl:w-1/3 xl:h-24 rounded-full outline-none focus:border-8 border-[#FA7268] p-8 text-4xl text-center font-bold' />
+            <input type="text" className='h-16 w-3/4 xl:w-1/3 xl:h-24 rounded-full outline-none focus:border-8 border-[#C62368] p-8 text-4xl text-center font-bold text-[#FA7268]' />
         </div>
      );
 }
