986f028f742b7e159fd8d22a6c6d6e2a96765694
FuzzyLogic123 <pat.e1@icloud.com>
2022-07-20 20:44:12 +1000

    add smooth scroll

--- src/Components/DownScrollButton.js
+++ src/Components/DownScrollButton.js
@@ -4,7 +4,11 @@ import { ReactComponent as DownArrow } from '../Icons/down-arrow.svg';
 
 function DownScrollButton() {
     return ( 
-            <DownArrow className='h-20 m-auto'/>
+        <DownArrow className='h-20 m-auto hover:scale-125 ease-in-out duration-300' onClick={()=>window.scroll({
+            left: 0,
+            top: 800,
+            behavior: 'smooth'
+        })}/>
      );
 }
 
